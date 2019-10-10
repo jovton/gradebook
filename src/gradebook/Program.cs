6d6d583c13371f38712ed6c42bdb5f78a303e092
Johan <jovton@gmail.com>
2019-10-10 23:07:35 +0200

    Hello jovton!

--- src/gradebook/Program.cs
+++ src/gradebook/Program.cs
@@ -6,7 +6,7 @@ namespace gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello jovton!");
         }
     }
 }
